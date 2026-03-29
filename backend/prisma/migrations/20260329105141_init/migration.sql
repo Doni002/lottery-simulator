bb90c06360a940b6e542a1162847900f2135f9d0
Donat Alpar <da@skytraders.lu>
2026-03-29 13:01:19 +0200

    connect db and create db schema

---
+++ backend/prisma/migrations/20260329105141_init/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `drawSpeed` INTEGER NOT NULL DEFAULT 750,
+    `randomSeedEnabled` BOOLEAN NOT NULL DEFAULT false,
+    `customNumbers` JSON NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Ticket` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `sessionId` VARCHAR(191) NOT NULL,
+    `numbers` JSON NOT NULL,
+    `isWinner` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Draw` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `sessionId` VARCHAR(191) NOT NULL,
+    `winningNumbers` JSON NOT NULL,
+    `drawDate` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Draw_sessionId_key`(`sessionId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Ticket` ADD CONSTRAINT `Ticket_sessionId_fkey` FOREIGN KEY (`sessionId`) REFERENCES `Session`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Draw` ADD CONSTRAINT `Draw_sessionId_fkey` FOREIGN KEY (`sessionId`) REFERENCES `Session`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
