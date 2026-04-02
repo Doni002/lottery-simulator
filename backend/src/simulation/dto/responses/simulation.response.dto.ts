ac06b30915f31badbc7a473b1006a55e0ee11260
Donat Alpar <da@skytraders.lu>
2026-04-02 15:53:22 +0200

    remove unused files and code from backend

--- backend/src/simulation/dto/responses/simulation.response.dto.ts
+++ backend/src/simulation/dto/responses/simulation.response.dto.ts
@@ -1,10 +1,5 @@
-import { Prisma, Session } from '@prisma/client';
-
-export type SessionWithRelations = Prisma.SessionGetPayload<{
-  include: { tickets: true; draws: true };
-}>;
+import { Session } from '@prisma/client';
 
 export type CreateSessionResponse = { session: Session };
-export type SessionDetailsResponse = { session: SessionWithRelations };
 export type StartSimulationResponse = { accepted: boolean; message: string };
 export type StopSimulationResponse = { accepted: boolean; message: string };
